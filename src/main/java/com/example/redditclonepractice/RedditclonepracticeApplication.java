5f73dbab6d04b2d7192c07bec492fa92a530e9d4
Christian Pirhalla <chrispir@udel.edu>
2021-01-28 22:10:53 -0500

    initial commit

---
+++ src/main/java/com/example/redditclonepractice/RedditclonepracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.redditclonepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditclonepracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditclonepracticeApplication.class, args);
+	}
+
+}
